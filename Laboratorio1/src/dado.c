5561aeeaf185055e4c51f558bb570d13510b7641
AleCast219 <jose_ale1999@hotmail.com>
2025-03-24 23:41:47 -0600

    Update dado.c
    Implementación de función de rotación de bits e implementación de generador aleatorio

--- Laboratorio1/src/dado.c
+++ Laboratorio1/src/dado.c
@@ -9,3 +9,19 @@ word __at 0x2007 __CONFIG = (_WDTE_OFF);
 void esperar(unsigned int t) {
     while (t--) for (unsigned int i = 0; i < 2000; i++);
 }
+
+// Rotación de bits a la izquierda, usada por el generador de números aleatorios
+uint8_t rotar_izq(uint8_t v, int k) {
+    return (v << k) | (v >> (8 - k));
+}
+// Estado interno para el generador de números aleatorios
+uint8_t estado[2] = { 0, 0xA3 };
+
+// Algoritmo Xorshift modificado para generar números pseudoaleatorios
+uint8_t siguiente(void) {
+    uint8_t r = estado[0] + estado[1];
+    estado[1] ^= estado[0];
+    estado[0] = rotar_izq(estado[0], 6) ^ estado[1] ^ (estado[1] << 1);
+    estado[1] = rotar_izq(estado[1], 3);
+    return r;
+}
